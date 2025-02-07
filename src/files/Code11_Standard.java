3b187fce2b1777bb5a3415929cea30eddbde6e92
BuYaoNiLiKai <1923667524@qq.com>
2025-02-07 11:21:03 +0800

    第二次

---
+++ src/files/Code11_Standard.java
@@ -0,0 +1,12 @@
+package files;
+
+import java.util.Scanner;
+
+public class Code11_Standard {
+    public static void main(String[] args) {
+
+        System.out.println(System.in.getClass());
+        System.out.println(System.out.getClass());
+        Scanner scanner = new Scanner(System.in);
+    }
+}
---
+++ src/files/Code12_CodeQuestion.java
@@ -0,0 +1,9 @@
+package files;
+
+public class Code12_CodeQuestion {
+    public static void main(String[] args) {
+
+        // 乱码问题
+        
+    }
+}

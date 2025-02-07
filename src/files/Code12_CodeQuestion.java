9c85d834becf27109aa75a68fe77da01fc6d757a
BuYaoNiLiKai <1923667524@qq.com>
2025-02-07 11:28:31 +0800

    第二次

--- src/files/Code12_CodeQuestion.java
+++ src/files/Code12_CodeQuestion.java
@@ -1,9 +1,22 @@
 package files;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Code12_CodeQuestion {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // 乱码问题
-        
+        FileInputStream fis = null;
+        String filePath = "hello.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line = "";
+        System.out.println("读取内容：");
+        line = br.readLine();
+        System.out.println(line);
+
+
     }
 }
